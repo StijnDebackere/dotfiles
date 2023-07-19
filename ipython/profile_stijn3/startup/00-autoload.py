173b1d4c37cfae782b5303d12993a61541db94e9
Stijn Debackere <stijndebackere@ip-10-106-112-131.us-east-2.compute.internal>
2023-07-19 10:21:16 -0600

    Remove astrophysics defaults.

--- ipython/profile_stijn3/startup/00-autoload.py
+++ ipython/profile_stijn3/startup/00-autoload.py
@@ -1,10 +1,6 @@
 from importlib import reload
 import sys
 
-import asdf
-import astropy.constants as c
-import astropy.units as u
-import h5py
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
--- zsh/zsh-custom/config.zsh
+++ zsh/zsh-custom/config.zsh
@@ -1,3 +1,5 @@
+export PATH="/usr/local/bin/:$PATH"
+
 # Mac finder apps set path
 launchctl setenv PATH $PATH
 
